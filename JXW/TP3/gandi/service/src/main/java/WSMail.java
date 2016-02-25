bbb8d17fbd2b6df3795e690dd02c6097c4bd8780
AmarOk <blin.sebastien@enconn.fr>
2016-02-25 18:34:38 +0100

    update service

--- JXW/TP3/gandi/service/src/main/java/WSMail.java
+++ JXW/TP3/gandi/service/src/main/java/WSMail.java
@@ -1,50 +1,84 @@
 package net.mail.ws.service;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
-@WebService(targetNamespace = "http://david.bromberg.fr/", name = "WSMail")
-@SOAPBinding(style = SOAPBinding.Style.RPC)
+import java.util.ArrayList;
+import javax.mail.*;
+import javax.mail.internet.*;
 
+@WebService(name = "WSMail", targetNamespace = "http://david.bromberg.fr/")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface WSMail {
+
+  /**
+  * Send a e-mail
+  * @param password
+  * @param smtpHost
+  * @param smtpPort
+  * @param from
+  * @param to
+  * @param username
+  * @param isSSL
+  */
   @WebMethod
   public void sendMail(
-  @WebParam(name = "smtpHost")
-  java.lang.String smtpHost,
-  @WebParam(name = "smtpPort")
+  @WebParam(name = "smtpHost", partName = "smtpHost")
+  String smtpHost,
+  @WebParam(name = "smtpPort", partName = "smtpPort")
   int smtpPort,
-  @WebParam(name = "from")
-  java.lang.String from,
-  @WebParam(name = "to")
-  java.lang.String to,
-  @WebParam(name = "username")
-  java.lang.String username,
-  @WebParam(name = "password")
-  java.lang.String password,
-  @WebParam(name = "isSSL")
-  boolean isSSL
-  );
+  @WebParam(name = "from", partName = "from")
+  String from,
+  @WebParam(name = "to", partName = "to")
+  String to,
+  @WebParam(name = "username", partName = "username")
+  String username,
+  @WebParam(name = "password", partName = "password")
+  String password,
+  @WebParam(name = "isSSL", partName = "isSSL")
+  boolean isSSL);
 
+  /**
+  * Get all mails
+  * @param password
+  * @param smtpHost
+  * @param smtpPort
+  * @param imapHost
+  * @param imapPort
+  * @param username
+  * @param isSSL
+  */
   @WebMethod
   public String checkMail(
-  @WebParam(name = "smtpHost")
-  java.lang.String smtpHost,
-  @WebParam(name = "smtpPort")
+  @WebParam(name = "smtpHost", partName = "smtpHost")
+  String smtpHost,
+  @WebParam(name = "smtpPort", partName = "smtpPort")
   int smtpPort,
-  @WebParam(name = "imapHost")
-  java.lang.String imapHost,
-  @WebParam(name = "imapPort")
+  @WebParam(name = "imapHost", partName = "imapHost")
+  String imapHost,
+  @WebParam(name = "imapPort", partName = "imapPort")
   int imapPort,
-  @WebParam(name = "username")
-  java.lang.String username,
-  @WebParam(name = "password")
-  java.lang.String password,
-  @WebParam(name = "isSSL")
+  @WebParam(name = "username", partName = "username")
+  String username,
+  @WebParam(name = "password", partName = "password")
+  String password,
+  @WebParam(name = "isSSL", partName = "isSSL")
   boolean isSSL
   );
 
+  /**
+  * Create a folder
+  * @param password
+  * @param smtpHost
+  * @param smtpPort
+  * @param imapHost
+  * @param imapPort
+  * @param username
+  * @param isSSL
+  * @param rep the name of the new directory
+  */
   @WebMethod
   public void createRep(
   @WebParam(name = "smtpHost")
@@ -63,5 +97,37 @@ public interface WSMail {
   boolean isSSL,
   @WebParam(name = "rep")
   String rep
+  );
+
+
+  /**
+  * Search mail where the subject contains the param subject
+  * @param password
+  * @param smtpHost
+  * @param smtpPort
+  * @param imapHost
+  * @param imapPort
+  * @param username
+  * @param isSSL
+  * @param subject
+  */
+  @WebMethod
+  public int searchBySubjectContains(
+  @WebParam(name = "smtpHost")
+  java.lang.String smtpHost,
+  @WebParam(name = "smtpPort")
+  int smtpPort,
+  @WebParam(name = "imapHost")
+  java.lang.String imapHost,
+  @WebParam(name = "imapPort")
+  int imapPort,
+  @WebParam(name = "username")
+  java.lang.String username,
+  @WebParam(name = "password")
+  java.lang.String password,
+  @WebParam(name = "isSSL")
+  boolean isSSL,
+  @WebParam(name = "subject")
+  java.lang.String subject
   );
 }
--- JXW/TP3/gandi/service/src/main/java/WSMailImpl.java
+++ JXW/TP3/gandi/service/src/main/java/WSMailImpl.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Properties;
 import java.util.Vector;
+import java.util.ArrayList;
 
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -15,28 +16,18 @@ import javax.jws.WebService;
 @WebService(endpointInterface = "net.mail.ws.service.WSMail", serviceName = "MyOwnMailAgent")
 public class WSMailImpl {
   public void sendMail (String smtpHost, int smtpPort,  String from, String to, String  username, String password,  boolean isSSL){
-    Properties props = new Properties();
 
-    if(isSSL) {
-      props.put("mail.transport.protocol", "smtps");
-      props.put("mail.smtps.auth", "true");
-    } else {
-      props.put("mail.transport.protocol", "smtp");
-      props.put("mail.smtps.auth", "false");
-    }
-
-    props.put("mail.smtp.host", smtpHost);
-
-    Session session = Session.getDefaultInstance(props);
-    session.setDebug(true);
+    Session session = getSession(smtpHost, isSSL);
 
     try {
+      //Create the mail
       MimeMessage message = new MimeMessage(session);
       message.setFrom(new InternetAddress(from));
       message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
       message.setSubject("Hello");
       message.setText("Hello World");
 
+      //Send the mail
       Transport tr;
       tr = session.getTransport();
       tr.connect(smtpHost, smtpPort, username, password);
@@ -52,24 +43,12 @@ public class WSMailImpl {
   }
 
   public void createRep (String smtpHost, int smtpPort,  String imapHost, int imapPort, String  username, String password, boolean isSSL, String rep){
-    Properties props = new Properties();
 
-    if(isSSL) {
-      props.put("mail.transport.protocol", "smtps");
-      props.put("mail.smtps.auth", "true");
-    } else {
-      props.put("mail.transport.protocol", "smtp");
-      props.put("mail.smtps.auth", "false");
-    }
-
-    props.put("mail.smtp.host", smtpHost);
-
-    Session session = Session.getDefaultInstance(props);
-    session.setDebug(true);
+    Session session = getSession(smtpHost, isSSL);
 
     try {
-        Store  store = session.getStore("imaps");
-        store.connect(imapHost, imapPort, username, password);
+      Store  store = session.getStore("imaps");
+      store.connect(imapHost, imapPort, username, password);
 
       //create the folder object and open it
       Folder defaultFolder = store.getDefaultFolder();
@@ -87,6 +66,74 @@ public class WSMailImpl {
   public String checkMail (String smtpHost, int smtpPort,  String imapHost, int imapPort,  String  username, String password,  boolean isSSL){
     String content = "";
 
+    Session session = getSession(smtpHost, isSSL);
+
+    try {
+      Store  store = session.getStore("imaps");
+      store.connect(imapHost, imapPort, username, password);
+
+      Folder emailFolder = store.getFolder("INBOX");
+      emailFolder.open(Folder.READ_ONLY);
+
+      // retrieve the messages from the folder in an array and print it
+      Message[] messages = emailFolder.getMessages();
+      System.out.println("messages.length---" + messages.length);
+
+      for (int i = 0, n = messages.length; i < n; i++) {
+        Message message = messages[i];
+        System.out.println("---------------------------------");
+        System.out.println("Email Number " + (i + 1));
+        System.out.println("Subject: " + message.getSubject());
+        System.out.println("From: " + message.getFrom()[0]);
+        System.out.println("Text: " + message.getContent().toString());
+
+        content += "\n----\nSubject:" + message.getSubject() + "\nFrom: " + message.getFrom()[0] + "\nText: " + message.getContent().toString();
+
+      }
+
+      //close the store and folder objects
+      emailFolder.close(false);
+      store.close();
+    }
+    catch (Exception e) {
+      System.out.println("MessagingException");
+    }
+    return content;
+  }
+
+  public int searchBySubjectContains(String smtpHost, int smtpPort, String imapHost, int imapPort, String username, String password, boolean isSSL, String subject) {
+    ArrayList<String> goodMessages = new ArrayList<String>();
+
+    Session session = getSession(smtpHost, isSSL);
+
+    try {
+      Store  store = session.getStore("imaps");
+      store.connect(imapHost, imapPort, username, password);
+
+      //create the folder object and open it
+      Folder emailFolder = store.getFolder("INBOX");
+      emailFolder.open(Folder.READ_ONLY);
+
+      // retrieve the messages from the folder check if the subject contains the string wanted
+      Message[] messages = emailFolder.getMessages();
+      for (int i = 0, n = messages.length; i < n; i++) {
+        Message message = messages[i];
+        if(message.getSubject().contains(subject))
+        goodMessages.add(message.getSubject());
+      }
+
+      //close the store and folder objects
+      emailFolder.close(false);
+      store.close();
+    }
+    catch (Exception e) {
+      System.out.println("MessagingException");
+    }
+    return goodMessages.size();
+  }
+
+  private Session getSession(String smtpHost, boolean isSSL)
+  {
     Properties props = new Properties();
 
     if(isSSL) {
@@ -102,38 +149,7 @@ public class WSMailImpl {
     Session session = Session.getDefaultInstance(props);
     session.setDebug(true);
 
-    try {
-
-        Store  store = session.getStore("imaps");
-        store.connect(imapHost, imapPort, username, password);
-
-      //create the folder object and open it
-      Folder emailFolder = store.getFolder("INBOX");
-      emailFolder.open(Folder.READ_ONLY);
-
-      // retrieve the messages from the folder in an array and print it
-      Message[] messages = emailFolder.getMessages();
-      System.out.println("messages.length---" + messages.length);
-
-      for (int i = 0, n = messages.length; i < n; i++) {
-         Message message = messages[i];
-         System.out.println("---------------------------------");
-         System.out.println("Email Number " + (i + 1));
-         System.out.println("Subject: " + message.getSubject());
-         System.out.println("From: " + message.getFrom()[0]);
-         System.out.println("Text: " + message.getContent().toString());
-
-         content += "\n----\nSubject:" + message.getSubject() + "\nFrom: " + message.getFrom()[0] + "\nText: " + message.getContent().toString();
-
-      }
-
-      //close the store and folder objects
-      emailFolder.close(false);
-      store.close();
-    }
-    catch (Exception e) {
-      System.out.println("MessagingException");
-    }
-    return content;
+    return session;
   }
+
 }
