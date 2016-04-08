e8d745a50fa4a648b3c6abeb0bfbcf8e89192faa
AmarOk <blin.sebastien@enconn.fr>
2016-04-08 11:25:48 +0200

    change directory

---
+++ SPP/TP34/src/Semaphore/SemaphoreImplementation.java
@@ -0,0 +1,47 @@
+package Semaphore;
+
+public class SemaphoreImplementation implements SemaphoreInterface {
+
+	//Count blocked Threads
+	int _blocked = 0;
+	
+	@Override
+	public synchronized void up()
+	{
+		//Avoid to deblock blocked threads if we release too much
+		if(_blocked != Integer.MAX_VALUE)
+			++_blocked;
+		if(_blocked <= 0)//Deblock one Thread is blocked
+			notify();
+	}
+
+	@Override
+	public synchronized void down()
+	{
+		//Avoid to block Threads if we acquire too much
+		if(_blocked != Integer.MIN_VALUE)
+			--_blocked;
+		
+		if (_blocked < 0) //wait if not available. Wait at the end, Mesa style
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	@Override
+	public synchronized int releaseAll() {
+		//Count blocked threads
+		int blocked = _blocked > 0 ? 0 : -_blocked;
+		//Deblock all
+		if(_blocked < 0)
+		{
+			notifyAll();
+			_blocked = 0; 
+		}
+		return blocked;
+	}
+
+}
---
+++ SPP/TP34/src/Semaphore/SemaphoreInterface.java
@@ -0,0 +1,20 @@
+package Semaphore;
+
+public interface SemaphoreInterface {
+
+	/**
+	 * Release Semaphore
+	 */
+	public void up();
+	
+	/**
+	 * Acquire Semaphore
+	 */
+	public void down();
+	
+	/**
+	 * Release all Threads
+	 * @return
+	 */
+	public int  releaseAll();
+}
