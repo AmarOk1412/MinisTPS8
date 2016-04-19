757b38109ad5a2f22cef95320a923ca0e17710d4
AmarOk <blin.sebastien@enconn.fr>
2016-04-19 18:09:18 +0200

    generate graph

--- SPP/TP5-6/src/Eratosthenes.java
+++ SPP/TP5-6/src/Eratosthenes.java
@@ -9,12 +9,67 @@ import org.junit.Test;
 
 public class Eratosthenes {
 	
-	public int Below = 100;
+	public int Below = 1000000;
 
-	//TODO Calculate time
 	@Test
 	public void sequentialCheck500kPrimeNumber() {
 		Integer limit = new Integer(Below);
+		PrimeNumberFactory pnf = new SequentialEratosthenesSieve();
+		long begin = System.currentTimeMillis();
+		ArrayList<Integer> primeNumber = pnf.getPrimes(limit);
+		long end = System.currentTimeMillis();
+		long duration = end - begin;
+		System.out.println("Durée : " + duration);
+		int numberPrimes = 0;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("primes-to-100k.txt")))
+		{
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				Integer lineToPrime = new Integer(line);
+				if(lineToPrime > limit) break;
+				++numberPrimes;
+				assertTrue(primeNumber.contains(lineToPrime));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	@Test
+	public void parallelCheck500kPrimeNumber() {
+		for(int limitThreads = 1; limitThreads <= 10; ++limitThreads)
+		{
+			Integer limit = new Integer(Below);
+			System.out.println(limitThreads + " threads");
+			PrimeNumberFactory pnf = new ParallelEratosthenesSieve(limitThreads);
+			long begin = System.currentTimeMillis();
+			ArrayList<Integer> primeNumber = pnf.getPrimes(limit);
+			long end = System.currentTimeMillis();
+			long duration = end - begin;
+			System.out.println("Durée : " + duration);
+			int numberPrimes = 0;
+			
+			try (BufferedReader br = new BufferedReader(new FileReader("primes-to-100k.txt")))
+			{
+				String line;
+	
+				while ((line = br.readLine()) != null) {
+					Integer lineToPrime = new Integer(line);
+					if(lineToPrime > limit) break;
+					++numberPrimes;
+					assertTrue(primeNumber.contains(lineToPrime));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
+	public void sequentialCheck100kPrimeNumber() {
+		Integer limit = new Integer(100000);
 		PrimeNumberFactory pnf = new SequentialEratosthenesSieve();
 		long begin = System.currentTimeMillis();
 		ArrayList<Integer> primeNumber = pnf.getPrimes(limit);
@@ -40,10 +95,11 @@ public class Eratosthenes {
 	}
 	
 	@Test
-	public void parallelCheck500kPrimeNumber() {
+	public void parallelCheck100kPrimeNumber() {
 		for(int limitThreads = 1; limitThreads <= 10; ++limitThreads)
 		{
-			Integer limit = new Integer(Below);
+			Integer limit = new Integer(100000);
+			System.out.println(limitThreads + " threads");
 			PrimeNumberFactory pnf = new ParallelEratosthenesSieve(limitThreads);
 			long begin = System.currentTimeMillis();
 			ArrayList<Integer> primeNumber = pnf.getPrimes(limit);
